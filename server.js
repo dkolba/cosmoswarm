1d9be0a09bba7550941a3455ba9a648539ffa203
Duncan Kolba <duncankolba@gmail.com>
2014-05-11 20:08:12 +0000

    Don't cache in st

--- server.js
+++ server.js
@@ -5,32 +5,7 @@ var http = require('http')
 var mount = st({
     path: "www/",
     url: "/", // defaults to path option
-    cache: {
-      fd: {
-        max: 1000, // number of fd's to hang on to
-        maxAge: 1000*60*60, // amount of ms before fd's expire
-      },
-
-      stat: {
-        max: 5000, // number of stat objects to hang on to
-        maxAge: 1000 * 60, // number of ms that stats are good for
-      },
-
-      content: {
-        max: 1024*1024*64, // how much memory to use on caching contents
-        maxAge: 1000 * 60 * 10, // how long to cache contents for
-      },
-
-      index: { // irrelevant if not using index:true
-        max: 1024 * 8, // how many bytes of autoindex html to cache
-        maxAge: 1000 * 60 * 10, // how long to store it for
-      },
-
-      readdir: { // irrelevant if not using index:true
-        max: 1000, // how many dir entries to cache
-        maxAge: 1000 * 60 * 10, // how long to cache them for
-      }
-    },
+    cache: false,
 
     // index: true, // auto-index
     // index: 'index.html', // use 'index.html' file as the index
