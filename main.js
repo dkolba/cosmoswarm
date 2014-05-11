69430bd8858cbdaad8dc50900d498b17f7fd67d1
Duncan Kolba <duncankolba@gmail.com>
2014-05-11 21:51:48 +0000

    Merged basic animation branch

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-var hammerjs = require ('hammerjs');
+var hammerjs = require ('hammerjs')
+  , canvas = document.getElementById('canvas')
+  , ctx = canvas.getContext('2d')
+  , img = new Image()   // Create new img element
+  , i = 0;
 
-console.log('Can touch this');
+img.src = '/img/ship_orange.svg'; // Set source path
+
+function draw() {
+  i = (i + 1) % 600;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(img, i, Math.sin(i*0.5), 100, 100);
+  window.requestAnimationFrame(draw);
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  draw();
+});
