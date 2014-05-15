aa78a87c7eb4f8cceda758a4a2f26611a7e82a0c
Duncan Kolba <duncankolba@gmail.com>
2014-05-15 20:12:23 +0000

    Fit canvas to window

--- main.js
+++ main.js
@@ -4,12 +4,15 @@ var hammerjs = require ('hammerjs')
   , img = new Image()   // Create new img element
   , i = 0;
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 img.src = '/img/ship_orange.svg'; // Set source path
 
 function draw() {
-  i = (i + 1) % 600;
+  i = (i + 1) % (canvas.width + 100);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(img, i, Math.sin(i*0.5), 100, 100);
+  ctx.drawImage(img, i-100, Math.sin(i*0.5), 100, 100);
   window.requestAnimationFrame(draw);
 }
 
