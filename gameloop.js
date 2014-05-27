cf86ba5c7d7c891b5618f9477ebb2d7dd356895f
Duncan Kolba <duncankolba@gmail.com>
2014-05-27 21:33:43 +0000

    Add requestAnimationFrame polyfill to gameloop.js

--- gameloop.js
+++ gameloop.js
@@ -1,10 +1,21 @@
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function( callback ){
+      window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
+
 var lastTime = (new Date()).getTime()
   , currentTime = 0
   , fps = 30
   , interval = 1000/fps;
 
 module.exports = function gameLoop(cb) {
-  window.requestAnimationFrame(function() {gameLoop(cb);});
+  window.requestAnimFrame(function() {gameLoop(cb);});
   currentTime = (new Date()).getTime();
   delta = (currentTime-lastTime);
 
