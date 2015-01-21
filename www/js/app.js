81aefa56595c515624a46b673714658fe9a71cd2
Duncan Kolba <duncankolba@gmail.com>
2015-01-21 22:34:26 +0000

    Basic movement of ship

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,32 @@
 var Phaser = require('phaser');
 
 window.onload = function() {
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create,
+    update: update});
+   var ship, cursors;
 
   function preload () {
-    game.load.image('logo', 'phaser.png');
+    game.load.image('ship', '../img/ship_orange.svg');
+    game.load.image('explosion', '../img/explosion.svg');
+    game.load.image('heart', '../img/heart.svg');
+    game.load.image('monster', '../img/monster.svg');
   }
 
   function create () {
-    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
+    cursors = game.input.keyboard.createCursorKeys();
+    ship = game.add.sprite(game.world.centerX, game.world.height-20, 'ship');
+    ship.anchor.setTo(0.5, 1);
+    game.physics.enable(ship, Phaser.Physics.ARCADE);
+  }
+
+  function update () {
+    ship.body.velocity.x=0;
+
+    if (cursors.left.isDown) {
+      ship.body.velocity.x = -300;
+    }
+    else if (cursors.right.isDown) {
+      ship.body.velocity.x = 300;
+    }
   }
 };
